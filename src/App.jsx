c8de690a522eede3392b2a561f7189bff9524da0
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-06-15 16:05:38 +0500

    except image everything is responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,24 @@ import logo from "./assets/logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="px-6 py-12">
+    <div className="px-6 py-12 max-w-md mx-auto sm:max-w-xl">
       <img className="h-10" src={logo} alt="vite logo" />
       <img
-        className="rounded-lg mt-6 shadow-sm"
+        className="rounded-lg mt-6 shadow-xl sm:mt-8"
         src={WorkImage}
         alt="image of a lady working on a beach"
       />
-      <h1 className="mt-6 text-3xl font-bold text-indigo-500">
-        You can work from anywhere.{" "}
+      <h1 className="mt-6 text-2xl font-bold text-indigo-500 sm:mt-8 sm:text-4xl">
+        You can work from anywhere. <br className="hidden lg:inline" />
         <span className="text-gray-900">Take advantage of it.</span>{" "}
       </h1>
-      <p className="text-gray-600 mt-2">
+      <p className="text-gray-600 mt-2 sm:text-lg">
         workcation helps you find work-friendly rentals in beautiful location so
         you can enjoy some nice weather even when you're not on vacation.
       </p>
-      <div className="mt-4">
-        <button className="inline-block px-5 py-3 bg-indigo-600 text-white rounded-lg shadow-sm hover:bg-indigo-400">
+      <div className="mt-4 sm:mt-6">
+        <button className="inline-block px-5 py-3 bg-indigo-600 text-white font-semibold rounded-lg shadow-sm hover:bg-indigo-400 uppercase tracking-widest">
           Book you escape
         </button>
       </div>
