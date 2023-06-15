ca3db31bfb81054bb9ae746d26b59c2987b2fedf
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-06-15 15:06:04 +0500

    done with basic design

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,25 @@ function App() {
 
   return (
     <div className="px-6 py-12">
-      <img src={logo} alt="vite logo" />
+      <img className="h-10" src={logo} alt="vite logo" />
       <img
-        className="rounded-lg mt-6"
+        className="rounded-lg mt-6 shadow-sm"
         src={WorkImage}
         alt="image of a lady working on a beach"
       />
-      <h1> Hello From the future </h1>
+      <h1 className="mt-6 text-3xl font-bold text-indigo-500">
+        You can work from anywhere.{" "}
+        <span className="text-gray-900">Take advantage of it.</span>{" "}
+      </h1>
+      <p className="text-gray-600 mt-2">
+        workcation helps you find work-friendly rentals in beautiful location so
+        you can enjoy some nice weather even when you're not on vacation.
+      </p>
+      <div className="mt-4">
+        <button className="inline-block px-5 py-3 bg-indigo-600 text-white rounded-lg shadow-sm hover:bg-indigo-400">
+          Book you escape
+        </button>
+      </div>
     </div>
   );
 }
